d9ba6ba199b65f95092df385fc62a6f0c9ca2d37
Marc Daya <marc@sinistral.net>
2016-05-23 12:12:16 -0600

    Introduce an abstraction layer for the backend.

---
+++ src/sagittariidae/fe/backend.cljs
@@ -0,0 +1,30 @@
+
+(ns sagittariidae.fe.backend)
+
+(def ^{:private true} db
+  {:samples {"P001-B001-C001-R001" [{:id 0 :method-id 0 :annotation "Ann0"}
+                                    {:id 1 :method-id 1 :annotation "Ann1"}]
+             "P001-B001-C001-R002" [{:id 0 :method-id 1 :annotation "Ann2"}]}
+   :stage-details {"P001-B001-C001-R001:0" [{:id 0 :file "scan-001.scn" :status :ready}
+                                            {:id 1 :file "scan-002.scn" :status :ready}
+                                            {:id 2 :file "scan-003.scn" :status :ready}]
+                   "P001-B001-C001-R001:1" [{:id 0 :file "strain.dat" :status :processing}]}
+   :methods {0 {:name "X-ray tomography" :type :scan}
+             1 {:name "Compression" :type :physical}
+             2 {:name "Strain" :type :physical}
+             3 {:name "Porosity" :type :analysis}}})
+
+(defonce ^{:private true} sample-methods
+  (:methods db))
+
+(defn sample-stages
+  [project-id sample-id]
+  (or (get-in db [:samples sample-id]) []))
+
+(defn stage-methods
+  []
+  (get db :methods))
+
+(defn stage-details
+  [project-id sample-id stage-id]
+  (get-in db [:stage-details (str sample-id ":" stage-id)]))
--- src/sagittariidae/fe/main.cljs
+++ src/sagittariidae/fe/main.cljs
@@ -3,24 +3,10 @@
   (:require [clojure.string :as str]
             [cljs.pprint :refer [cl-format]]
             [reagent.core :as reagent]
-            [cljsjs.react-bootstrap]))
+            [cljsjs.react-bootstrap]
+            [sagittariidae.fe.backend :as be]))
 
 ;; --------------------------------------------------------------- state --- ;;
-
-(def db {:samples {"P001-B001-C001-R001" [{:id 0 :method-id 0 :annotation "Ann0"}
-                                          {:id 1 :method-id 1 :annotation "Ann1"}]
-                   "P001-B001-C001-R002" [{:id 0 :method-id 1 :annotation "Ann2"}]}
-         :stage-details {"P001-B001-C001-R001:0" [{:id 0 :file "scan-001.scn" :status :ready}
-                                                  {:id 1 :file "scan-002.scn" :status :ready}
-                                                  {:id 2 :file "scan-003.scn" :status :ready}]
-                         "P001-B001-C001-R001:1" [{:id 0 :file "strain.dat" :status :processing}]}
-         :methods {0 {:name "X-ray tomography" :type :scan}
-                   1 {:name "Compression" :type :physical}
-                   2 {:name "Strain" :type :physical}
-                   3 {:name "Porosity" :type :analysis}}})
-
-(defonce sample-methods
-  (:methods db))
 
 (defn null-sample-state
   []
@@ -76,13 +62,12 @@
 
 (defn component:sample-search
   [sample-detail sample-stage-detail]
-  (component:text-input-action (fn [s]
-                                 (reset! sample-detail (null-sample-state))
-                                 (reset! sample-stage-detail (null-sample-stage-detail-state))
-                                 (.debug js/console
-                                         (str "Fetching details for sample " s))
-                                 (reset! sample-detail {:id s :stages (or (get-in db [:samples s]) [])}))
-                               "Sample ID"))
+  (letfn [(button-action [sample-id]
+            (reset! sample-detail (null-sample-state))
+            (reset! sample-stage-detail (null-sample-stage-detail-state))
+            (.debug js/console (str "Fetching details for sample " sample-id))
+            (reset! sample-detail {:id sample-id :stages (be/sample-stages nil sample-id)}))]
+    (component:text-input-action button-action "Sample ID")))
 
 (defn component:sample-stage-table
   [spec state]
@@ -105,30 +90,30 @@
         sample-stage-detail-state (reagent/atom (null-sample-stage-detail-state))]
     (add-component [component:sample-search sample-state sample-stage-detail-state]
                    "sample-search-bar")
-    (let [spec {:method-id
-                {:label "Method"
-                 :data-fn (fn [x]
-                            (:name (get sample-methods x)))}
-                :annotation
-                {:label "Annotation"}
-                :xref
-                {:label "Cross reference"}
-                :id
-                {:label ""
-                 :data-fn (fn [stage-id]
-                            (let [sample-id (:id @sample-state)]
-                              [:button.btn.btn-default {:type "button"
-                                                        :on-click (fn [_]
-                                                                    (.debug js/console (cl-format nil "Retrieving details of stage ~a for sample ~a" stage-id sample-id))
-                                                                    (reset! sample-stage-detail-state {:stage-id stage-id
-                                                                                                       :stage-details (get-in db [:stage-details (str sample-id ":" stage-id)])}))}
-                               [:span.glyphicon.glyphicon-expand]]))}}]
+    (let [method-data-fn
+          (fn [x]
+            (:name (get (be/stage-methods) x)))
+          id-btn-action
+          (fn [sample-id stage-id]
+            (.debug js/console (cl-format nil "Retrieving details of stage ~a for sample ~a" stage-id sample-id))
+            (reset! sample-stage-detail-state
+                    {:stage-id stage-id
+                     :stage-details (be/stage-details nil sample-id stage-id)}))
+          id-data-fn
+          (fn [stage-id]
+            (let [sample-id (:id @sample-state)]
+              [:button.btn.btn-default {:type "button"
+                                        :on-click #(id-btn-action sample-id stage-id)}
+               [:span.glyphicon.glyphicon-expand]]))
+          spec
+          {:method-id {:label "Method" :data-fn method-data-fn}
+           :annotation {:label "Annotation"}
+           :xref {:label "Cross reference"}
+           :id {:label "" :data-fn id-data-fn}}]
       (add-component [component:sample-stage-table spec sample-state]
                      "sample-detail-table"))
-    (let [spec {:file
-                {:label "File"}
-                :status
-                {:label "Status"
-                 :data-fn str}}]
+    (let [spec
+          {:file {:label "File"}
+           :status {:label "Status" :data-fn str}}]
       (add-component [component:sample-stage-details-table spec sample-stage-detail-state]
                      "sample-stage-detail-table"))))
